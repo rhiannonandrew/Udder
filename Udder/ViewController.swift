97cfbe1a42ed18fda3b5e06f3460b7a0228d4826
Rhiannon Andrew <rhenry872@gmail.com>
2020-08-31 15:40:04 +0100

    Add viewWillAppear to main ViewController

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // <#code#>
+    }
 
 }
 
